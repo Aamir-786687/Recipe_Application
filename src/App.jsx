249406d1ed2875e8014fd6383aab183cc84e79ac
Aamir Ahmad <aamir557ahmad@gmail.com>
2025-04-02 18:16:00 +0530

    Recipe App added

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import Recipes from './pages/Recipes';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Footer from './pages/Footer';
-import Navbar from './Components/navbar';
+import Navbar from "./Components/Navbar";
+
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/register" element={<Register />} />
