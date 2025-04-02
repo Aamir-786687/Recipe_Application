b2908232dc7243cecb62d3df1de7d755f9f4013b
Aamir Ahmad <aamir557ahmad@gmail.com>
2025-04-02 18:23:33 +0530

    Recipe App added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Recipes from './pages/Recipes';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Footer from './pages/Footer';
-// import Navbar from "./Components/Navbar";
 import Navbarrr from './Components/Navbarrr';
-
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Navbarrr />
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
